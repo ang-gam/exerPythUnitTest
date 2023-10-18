d67f9ac4ca0861db03a40e3f3456b83612ce705c
ang-gam <143750241+ang-gam@users.noreply.github.com>
2023-10-18 00:08:31 -0600

    Add files via upload

--- employee.py
+++ employee.py
@@ -1,8 +1,6 @@
 import requests
 
-
 class Employee:
-    
 
     raise_amt = 1.05
 
@@ -22,7 +20,7 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
-    def monthly_schedule(self, month):
+   def monthly_schedule(self, month):
         response = requests.get(f'http://company.com/{self.last}/{month}')
         if response.ok:
             return response.text
